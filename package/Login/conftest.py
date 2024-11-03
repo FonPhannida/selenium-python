1a2db5902ab3b9df1083215bf7ce450e1b23d97b
fonphannida <jphan1@hotmail.com>
2024-11-03 16:11:33 +0700

    update conftest headless chrome

--- package/Login/conftest.py
+++ package/Login/conftest.py
@@ -3,13 +3,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+#headless chrome
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 @pytest.fixture(autouse=True, scope='class')
 def setup(request):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+    options = Options()
+    options.add_argument('--headless')
+    #normal browder
+    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    #headless is mean browser will not sure run step on moniter
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     request.cls.driver = driver
 
     yield
